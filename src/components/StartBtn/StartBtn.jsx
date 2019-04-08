0a53033252578ee082f3f035a447262f9ae2bbd7
Asim Zaidi <asimzaidih@gmail.com>
2019-04-08 15:20:38 -0700

    child class updating parent state handler :construction:

---
+++ src/components/StartBtn/StartBtn.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class StartBtn extends Component {
+
+    render() {
+        return (
+        <div className='center main-content'>
+            <button className='btn_primary red-grad-bg'>START</button>
+            {/* onclick change the parent state to startClickedTrue */}
+            {/* in render: if startClick: show Timer */}
+        </div>
+        )
+    }
+}
+
+export default StartBtn;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import '../../styles/home.scss';
-
+import StartBtn from '../../components/StartBtn';
 
 class Home extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.handler = this.handler.bind(this)
+    }
+
+    handler() {
+        this.setState({
+          someVar: someValue
+        })
+    }
+
     render() {
         return (
             <div className='home'>
-            <i className="fas fa-cog red-grad-color"></i>
-            <div className='center main-content'>
-                <button className='btn_primary red-grad-bg'>START</button>
-            </div>
+                <i className="fas fa-cog red-grad-color"></i>
+                <StartBtn handler = {this.handler}/>
             </div>
         )
     }
